a673d402c524839bebead0272478725774c8ba75
TaeHong Kim <artoflunar@gmail.com>
2024-05-17 14:20:33 +0900

    다른 조건의 TC 추가

--- BaseballTest/test.cpp
+++ BaseballTest/test.cpp
@@ -27,5 +27,43 @@ TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
 	Baseball game;
 	GuessResult result = game.guess("123");
 
+	EXPECT_EQ(true, result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
 
+TEST_F(BaseballFixture, ReturnSolvedResultIf2Strikes) {
+	Baseball game;
+	GuessResult result = game.guess("124");
+	EXPECT_EQ(false, result.solved);
+	EXPECT_EQ(2, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIf1Strike) {
+	Baseball game;
+	GuessResult result = game.guess("154");
+
+	EXPECT_EQ(false, result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIf1Strike1Ball) {
+	Baseball game;
+	GuessResult result = game.guess("142");
+
+	EXPECT_EQ(false, result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
+}
+
+
+TEST_F(BaseballFixture, ReturnSolvedResultIf1Ball) {
+	Baseball game;
+	GuessResult result = game.guess("451");
+
+	EXPECT_EQ(false, result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(1, result.balls);
 }
