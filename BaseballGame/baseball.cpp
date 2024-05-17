6ecb65fb7dfe9ec402e00da75fb797a45d3f7231
TaeHong Kim <artoflunar@gmail.com>
2024-05-17 13:55:11 +0900

    테스트 코드 추가

--- BaseballGame/baseball.cpp
+++ BaseballGame/baseball.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
 class Baseball {
 public:
+	void guess(string str) {
+		if (str.length() <= 2)
+			throw length_error;
 
+
+	}
 };
 
 int main()
